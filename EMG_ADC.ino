d8eacd573c7b5050b8637d6bc56e19d87178b7c1
paul-was-here <paul.g.kullmann@gmail.com>
2024-12-10 19:30:13 -0500

    Add files via upload

--- EMG_ADC.ino
+++ EMG_ADC.ino
@@ -1,24 +1,14 @@
-//ADC for EMG output
-#include <stdio.h>
+// basic code for acquisition of analog data from EMG and conversion to readable voltage value
+// no additional data processing
 #include <math.h>
-
-int DataPin = A0;
-int digitalValue = 0;
-float analogValue = 0.00;
-int DataRefreshDelay = 10000; //ms
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int Counter = 0;
-    while(Counter <= 10000) {
-      digitalValue = analogRead(DataPin);
-      analogValue = (digitalValue * 5)/1023;
-      printf("%f\n",analogValue);
-      delay(10);
-      Counter += 10;
-    }
-  printf("10 sec");
+  float sensorValue = analogRead(A1);
+  float voltageValue = sensorValue*5.00/1023.00;
+  Serial.println(voltageValue);
+  delay(10);
 }
